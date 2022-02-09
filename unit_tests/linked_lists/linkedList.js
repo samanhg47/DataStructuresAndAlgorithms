92e4f883b71de24902652b58c7824b76fefbb539
Sam'an <samangriffiths47@gmail.com>
2022-02-09 16:42:33 -0600

    prepending test

--- unit_tests/linked_lists/linkedList.js
+++ unit_tests/linked_lists/linkedList.js
@@ -7,12 +7,12 @@ class Node {
 class LinkedList {
   constructor(value) {
     this.set = {}
-    this.addValue(value)
+    this._addValue(value)
     this.head = new Node(value, null)
     this.tail = this.head
     this.length = 1
   }
-  checkIndex(index, bool = false) {
+  _checkIndex(index, bool = false) {
     if (
       typeof index === 'number' &&
       index >= 0 &&
@@ -28,13 +28,13 @@ class LinkedList {
     }
     return false
   }
-  addValue(value) {
+  _addValue(value) {
     if (typeof value === 'object') {
       value = JSON.stringify(value)
     }
     this.set[value] ? this.set[value]++ : (this.set[value] = 1)
   }
-  deleteValue(value) {
+  _deleteValue(value) {
     if (typeof value === 'object') {
       value = JSON.stringify(value)
     }
@@ -46,8 +46,8 @@ class LinkedList {
     }
     return this.set[value] ? true : false
   }
-  nodeAt(index) {
-    if (this.checkIndex(index, true)) {
+  _nodeAt(index) {
+    if (this._checkIndex(index, true)) {
       let node = this.head
       let counter = 0
       while (counter < index) {
@@ -56,7 +56,7 @@ class LinkedList {
       }
       return node
     }
-    undefined
+    return undefined
   }
   asArray() {
     let node = this.head
@@ -70,7 +70,7 @@ class LinkedList {
     return arr
   }
   append(value) {
-    this.addValue(value)
+    this._addValue(value)
     const newNode = new Node(value, null)
     this.tail.next = newNode
     this.tail = newNode
@@ -78,21 +78,21 @@ class LinkedList {
     return this
   }
   prepend(value) {
-    this.addValue(value)
+    this._addValue(value)
     const newNode = new Node(value, this.head)
     this.head = newNode
     this.length++
     return this
   }
   insert(value, index) {
-    if (this.checkIndex(index)) {
+    if (this._checkIndex(index)) {
       if (index === 0) {
         return this.prepend(value)
       } else if (index < this.length) {
-        let node = this.nodeAt(index - 1)
+        let node = this._nodeAt(index - 1)
         const newNode = new Node(value, node.next)
         node.next = newNode
-        this.addValue(value)
+        this._addValue(value)
         this.length++
         return this
       }
@@ -101,7 +101,7 @@ class LinkedList {
     return 'Invalid Node Index'
   }
   remove(index) {
-    if (this.checkIndex(index, true)) {
+    if (this._checkIndex(index, true)) {
       let val
       if (index === 0) {
         const newHead = this.head.next
@@ -109,34 +109,25 @@ class LinkedList {
         delete this.head
         this.head = newHead
       } else {
-        let node = this.nodeAt(index - 1)
+        let node = this._nodeAt(index - 1)
         const newNext = node.next.next
         val = node.next.value
         delete node.next
         node.next = newNext
       }
-      this.deleteValue(val)
+      this._deleteValue(val)
       this.length--
       return this
     }
     return 'Invalid Node Index'
   }
   valueAt(index) {
-    if (this.checkIndex(index, true)) {
-      let node = this.nodeAt(index)
+    if (this._checkIndex(index, true)) {
+      let node = this._nodeAt(index)
       return node.value
     }
     return undefined
   }
 }
-
-const list = new LinkedList('4')
-const obj = {
-  set: { 4: 1 },
-  head: { value: '4', next: null },
-  tail: { value: '4', next: null },
-  length: 1
-}
-console.log(JSON.stringify(list) == JSON.stringify(obj))
 
 module.exports = LinkedList
--- unit_tests/linked_lists/linkedLists.test.js
+++ unit_tests/linked_lists/linkedLists.test.js
@@ -1,13 +1,34 @@
 const LinkedList = require('./linkedList')
 
-const linkedList = new LinkedList('4')
-const obj = {
-  set: { 4: 1 },
-  head: { value: '4', next: null },
-  tail: { value: '4', next: null },
-  length: 1
+function copyObj(o) {
+  return JSON.parse(JSON.stringify(o))
 }
 
-test('Properly creates linked list', () => {
-  expect(JSON.stringify(linkedList)).toBe(JSON.stringify(obj))
+// First Test
+test('properly creates linked list', () => {
+  const list = new LinkedList('4')
+  const obj = {
+    set: { 4: 1 },
+    head: { value: '4', next: null },
+    tail: { value: '4', next: null },
+    length: 1
+  }
+  expect(JSON.stringify(list)).toBe(JSON.stringify(obj))
+})
+
+// Second Test
+test('Properly prepends value', () => {
+  const carla = { name: 'Carla' }
+  const list2 = new LinkedList('4').prepend(carla)
+  const name = JSON.stringify(carla)
+  const obj = {
+    set: { 4: 1 },
+    head: { value: '4', next: null },
+    tail: { value: '4', next: null },
+    length: 1
+  }
+  obj.set[name] = 1
+  obj.head = { value: { name: 'Carla' }, next: obj.tail }
+  obj.length += 1
+  expect(JSON.stringify(list2.set)).toBe(JSON.stringify(obj.set))
 })
