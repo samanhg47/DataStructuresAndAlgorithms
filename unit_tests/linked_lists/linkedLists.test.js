3cd6b53636f3398e396ca53a09cab6c19f5a0ffa
Sam'an <samangriffiths47@gmail.com>
2022-02-09 17:26:22 -0600

    insertion test

--- unit_tests/linked_lists/linkedLists.test.js
+++ unit_tests/linked_lists/linkedLists.test.js
@@ -12,19 +12,19 @@ test('properly creates linked list', () => {
 
 test('Properly prepends value', () => {
   const carla = { name: 'Carla' }
-  const list2 = new LinkedList('4').prepend(carla)
+  const list = new LinkedList('4').prepend(carla)
   const obj = {
     head: { value: { name: 'Carla' }, next: { value: '4', next: null } },
     tail: { value: '4', next: null },
     length: 2
   }
-  expect(JSON.stringify(list2)).toBe(JSON.stringify(obj))
+  expect(JSON.stringify(list)).toBe(JSON.stringify(obj))
 })
 
 test('Properly appends value', () => {
   const carla = { name: 'Carla' }
   const carla2 = { name: 'Carla' }
-  const list2 = new LinkedList('4').prepend(carla).append(carla2)
+  const list = new LinkedList('4').prepend(carla).append(carla2)
   const obj = {
     head: {
       value: { name: 'Carla' },
@@ -33,5 +33,23 @@ test('Properly appends value', () => {
     tail: { value: { name: 'Carla' }, next: null },
     length: 3
   }
-  expect(JSON.stringify(list2.set)).toBe(JSON.stringify(obj.set))
+  expect(JSON.stringify(list)).toBe(JSON.stringify(obj))
+})
+
+test('Properly inserts value', () => {
+  const carla = { name: 'Carla' }
+  const carla2 = { name: 'Carla' }
+  const list = new LinkedList('4').prepend(carla).append(carla2).insert(146, 2)
+  const obj = {
+    head: {
+      value: { name: 'Carla' },
+      next: {
+        value: '4',
+        next: { value: 146, next: { value: { name: 'Carla' }, next: null } }
+      }
+    },
+    tail: { value: { name: 'Carla' }, next: null },
+    length: 4
+  }
+  expect(JSON.stringify(list)).toBe(JSON.stringify(obj))
 })
