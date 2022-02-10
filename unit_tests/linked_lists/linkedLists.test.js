9cab8a055d1edf51bb2d4688000bfcc732cf42d9
Sam'an <samangriffiths47@gmail.com>
2022-02-09 19:01:19 -0600

    value at index test

--- unit_tests/linked_lists/linkedLists.test.js
+++ unit_tests/linked_lists/linkedLists.test.js
@@ -72,24 +72,24 @@ test('Properly Mutates Values', () => {
   expect(JSON.stringify(list)).toBe(JSON.stringify(obj))
 })
 
+const arr = [carla, '4', 146, carla2]
+const list = new LinkedList('4').prepend(carla).append(carla2).insert(2, 146)
+
 test('Properly Converts List To Array', () => {
-  const list = new LinkedList('4')
-    .prepend(carla)
-    .append(carla2)
-    .insert(2, 146)
-    .asArray()
-  const arr = [carla, '4', 146, carla2]
-  expect(JSON.stringify(list)).toBe(JSON.stringify(arr))
+  expect(JSON.stringify(list.asArray())).toBe(JSON.stringify(arr))
 })
 
 test('Properly Converts List To Hash Table', () => {
-  const list = new LinkedList('4')
-    .prepend(carla)
-    .append(carla2)
-    .insert(2, 146)
-    .hashed()
-  expect(list.get(carla)).toBe(1)
-  expect(list.get(carla2)).toBe(1)
-  expect(list.get(146)).toBe(1)
-  expect(list.get('4')).toBe(1)
+  expect(JSON.stringify([...list.hashed().keys()])).toBe(JSON.stringify(arr))
+  expect(list.hashed().get(carla)).toBe(1)
+  expect(list.hashed().get(carla2)).toBe(1)
+  expect(list.hashed().get(146)).toBe(1)
+  expect(list.hashed().get('4')).toBe(1)
+})
+
+test('Returns Correct Value At Selected Index', () => {
+  expect(list.valueAt(0)).toBe(carla)
+  expect(list.valueAt(1)).toBe('4')
+  expect(list.valueAt(2)).toBe(146)
+  expect(list.valueAt(3)).toBe(carla2)
 })
